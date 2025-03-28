d0e9c879630904ef63e5254ec307bbfb2a8e8e50
arunveguru <arunveguru2005@gmail.com>
2025-03-28 23:30:05 +0530

    Prepare for deployment

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import Main from "./pages/Main";
 
 export default function App() {
     return (
-        <Router>
+        <Router basename="/Local-Event-Explorer">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/main" element={<Main />} />
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Local-Event-Explorer/', 
+  base: '/Local-Event-Explorer/', // Set the base path to match your GitHub repository name
   plugins: [react()],
 });
